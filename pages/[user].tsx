20728c4c3a311c0655f1c500e6775bf645c49035
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-04-25 14:37:08 +0100

    Completed pages and major components of the UI

---
+++ pages/[user].tsx
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import {Avatar, IconButton, Input} from "@chakra-ui/react";
+import {BsFillSendFill} from "react-icons/bs"
+
+
+function ReceivingMsgCard() {
+    return <div className={"bg-blue-700 flex flex-col w-fit max-w-[400px] p-2 text-white rounded-r-lg rounded-b-lg"}>
+        <p className={"whitespace-pre-line"}>Hello there loem</p>
+        <small className={"self-end text-gray-400"}>02:30</small>
+    </div>;
+}
+
+function SendingMsgCard() {
+    return <div
+        className={"bg-blue-500 flex flex-col w-fit max-w-[400px] p-2 text-white rounded-l-lg rounded-b-lg self-end"}>
+        <p className={"whitespace-pre-line"}>Hello there loem </p>
+        <small className={"self-end text-gray-400"}>02:30</small>
+    </div>;
+}
+
+const User = () => {
+    return (
+        <div className="bg-blue-950 min-h-screen">
+            <Head>All Chats</Head>
+            <main className="min-h-full">
+                <header className={"fixed top-0 left-0 z-10 mb-5 w-full bg-white p-2 text-xl font-semibold mb-2"}>
+                    <div className={"flex gap-2 items-center"}>
+                        <Avatar name={"Litmus Zhang"} size={"sm"}/>
+                        <p>Litmus Zhang</p>
+                    </div>
+                </header>
+                <div className={"p-2 py-4 flex flex-col gap-1 mt-10 "}>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                    <ReceivingMsgCard/>
+                    <SendingMsgCard/>
+                </div>
+                <div className={"fixed bottom-0 bg-blue-800 p-3 w-full flex gap-2 text-white"}>
+                    <Input variant={"filled"} placeholder={"Type a message"}/>
+                    <IconButton colorScheme={"blue"} className={"bg-blue-700 shadow-md"} aria-label={"Send Message"}
+                                icon={<BsFillSendFill/>}/>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default User
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
-import type { NextPage } from 'next'
+import type {NextPage} from 'next'
+import Head from "next/head";
+import {Avatar} from "@chakra-ui/react";
 
+
+function ProfileCard() {
+    return <div
+        className={"flex cursor-pointer gap-2 p-3 items-center justify-between text-white border-b border-gray-700 w-full"}>
+        <div className={"flex items-center gap-2"}>
+            <Avatar name={"Litmus Zhang"}/>
+            <p className={"text-md"}>
+                Litmus Zhang 🎉
+            </p>
+        </div>
+        <small className={"text-gray-500 font-medium"}>
+            11:00
+        </small>
+    </div>;
+}
 
 const Home: NextPage = () => {
-  return (
-      <div className="text-2xl font-bold">
-          Hello world
-      </div>
-  )
+    const list = new Array(20).fill(0).map((_, i) => i + 1)
+    return (
+        <div className="bg-blue-950 min-h-full">
+            <Head>All Chats</Head>
+            <main className="">
+                <header className={"fixed top-0 left-0 z-10 mb-5 w-full bg-white p-2 text-xl font-semibold mb-2"}>
+                    💎 Friend Zone
+                </header>
+                <div className={"p-2 mt-10"}>
+                    {
+                        list.map(i => <ProfileCard key={i}/>
+                        )
+                    }
+                </div>
+            </main>
+        </div>
+    )
 }
 
 export default Home
